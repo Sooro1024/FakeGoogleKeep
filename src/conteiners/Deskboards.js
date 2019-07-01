81671b171a9510739e96cf5535618d892f5312ee
Suren <suren@Mac-mini-Admin.local>
2019-07-01 18:26:30 +0400

    mnac desk sarqel

--- src/conteiners/Deskboards.js
+++ src/conteiners/Deskboards.js
@@ -1,45 +1,43 @@
-import React from 'react';
-import { CRETATE_DESK } from '../actions/getDataAction';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import {
-	Card,
-	CardHeader,
-	CardContent,
-	CircularProgress
-} from '@material-ui/core';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+  Card,
+  CardHeader,
+  CardContent,
+  CircularProgress
+} from "@material-ui/core";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const Deskboards = ({ deskCollection, requested }) => {
-	if (requested) {
-		return deskCollection.map(el => (
-			<Card key={el.id}>
-				<CardHeader>{el.deskAuthor}</CardHeader>
-				<CardContent>{el.deskName}</CardContent>
-			</Card>
-		));
-	} else {
-		return <CircularProgress />;
-	}
+  if (requested) {
+    return deskCollection.map(el => (
+      <Card key={el.id}>
+        <CardHeader>{el.deskAuthor}</CardHeader>
+        <CardContent>{el.deskName}</CardContent>
+      </Card>
+    ));
+  }
+  return <CircularProgress />;
 };
 
 const MapDispatchToProps = {
-	// CRETATE_DESK
+  // CRETATE_DESK
 };
 
 const MapStateToProps = state => ({
-	deskCollection: state.firestoreReducer.ordered.deskCollection,
-	requested: state.firestoreReducer.status.requested.deskCollection
+  deskCollection: state.firestoreReducer.ordered.deskCollection,
+  requested: state.firestoreReducer.status.requested.deskCollection
 });
 
 export default compose(
-	connect(
-		MapStateToProps,
-		MapDispatchToProps
-	),
-	firestoreConnect([
-		{
-			collection: 'deskCollection' // here wy tipe the collection name in our firestore colectione
-		}
-	])
+  connect(
+    MapStateToProps,
+    MapDispatchToProps
+  ),
+  firestoreConnect([
+    {
+      collection: "deskCollection" // here wy tipe the collection name in our firestore colectione
+    }
+  ])
 )(Deskboards);
